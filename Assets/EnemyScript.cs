e818922cee64d5b29a924b77fa63977e0ea69fd6
dan <dnguyen3343@student.pps.net>
2024-05-15 10:00:57 -0700

    Made some progress

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
+[SerializeField]
+private EnemyData data;
+
+private float chosenLocation;
+private int maxHealth;
+private int currentHealth;
+private int damage;
+private float speed;
+private Vector2 target;
     // Start is called before the first frame update
     void Start()
     {
-        
+        chosenLocation = 0;
+        maxHealth = data.hp;
+        currentHealth = data.hp;
+        damage = data.damage;
+        speed = data.speed; 
     }
 
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(chosenLocation);
+        if (transform.position.x == chosenLocation){ 
+            chosenLocation = Random.Range(-8f,8f);
+        }
+        target = new Vector2(chosenLocation, transform.position.y);
+        transform.position = Vector2.MoveTowards(transform.position, (target) ,speed *Time.deltaTime);
         
     }
 }

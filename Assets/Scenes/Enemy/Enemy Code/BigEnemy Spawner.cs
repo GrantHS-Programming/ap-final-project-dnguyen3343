a4a4da9693a65e4f90010bef7d2c88728feab48f
dan <dnguyen3343@student.pps.net>
2024-05-23 09:49:27 -0700

    I just did some sprite making and made a lot of progress on the code

---
+++ Assets/Scenes/Enemy/Enemy Code/BigEnemy Spawner.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BigEnemySpawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bigEnemyPrefab;
+
+
+    [SerializeField]
+    private float bigEnemyInterval = 21f;
+
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(bigEnemyInterval,bigEnemyPrefab));
+    }
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        Vector3 temp = new Vector3();
+        temp = spawnCoords();
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(temp.x,temp.y,temp.z),Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval,enemy));
+    }
+
+   public int whichRow()
+    {
+        int thisRow = (int) ((Random.Range(1,4)));
+        return thisRow;
+    }
+    public int leftOrRight()
+    {
+        int x = (int) ((Random.Range(0,2)));
+        return x;
+    }
+    public Vector3 spawnCoords()
+    {
+        int thisRow = whichRow();
+        int fromWhere = leftOrRight();
+        float x;
+        float y;
+        float z;
+
+        if (thisRow == 1)
+        {
+            y = -0.25f;  
+            z = 5f; 
+        }
+        else if (thisRow == 2)
+        {
+            y = 1.75f;
+            z = 7f;
+        }
+        else
+        {
+            y = 3.75f;
+            z = 9f;
+        }
+        if (fromWhere == 0)
+        {
+            x = -9f;
+        }
+        else 
+        {
+            x = 9f;
+        }
+        Vector3 coords = new Vector3(x,y,z);
+
+
+
+        return coords;
+    }
+}
---
+++ Assets/Scenes/Player/Player Code/Follow Cursor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCursor : MonoBehaviour
+{
+    private float turnAngle;
+    public bool isBurstActive = false;
+    public bool isThisThePlayer = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isBurstActive || isThisThePlayer)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+            Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+            transform.up = direction;
+        }
+    }
+}
